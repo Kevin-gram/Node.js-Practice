f9b9eb78a7e66a31ada8c3662407e4f2da39db77
Kevin-gram <k.nyiringan@alustudent.com>
2024-08-19 17:00:26 +0200

    getting data using fs

--- modules.js
+++ modules.js
@@ -1,2 +1,10 @@
+const { error } = require("console");
 const { people, cars } = require("./people");
+const fs = require("fs");
+fs.readFile("./blog1.txt", (error, data) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log(data.toString());
+});
 console.log(people, cars);
