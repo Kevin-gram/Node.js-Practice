4fe6976f0ffb7d78cffc58413296a61ea713c3ff
Kevin-gram <k.nyiringan@alustudent.com>
2024-08-21 14:04:09 +0200

    creating a request that returns html

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const http = require("http");
+const fs = require("fs");
 const server = http.createServer((req, resp) => {
-  console.log("your server is being created");
+  resp.setHeader("Content-Type", "text/html");
+  fs.readFile("./views/index.html", (error, data) => {
+    if (error) {
+      console.log(error);
+    } else {
+      resp.write(data);
+      resp.end();
+    }
+  });
 });
+
 server.listen(3000, "localhost", () => {
   console.log("listening for the request on the port 3000");
 });
