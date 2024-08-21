4fa814ce1c83f3ae3cfe472966b2b57cef7c33b6
Kevin-gram <k.nyiringan@alustudent.com>
2024-08-21 14:49:45 +0200

    routing

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const http = require("http");
 const fs = require("fs");
+
 const server = http.createServer((req, resp) => {
   resp.setHeader("Content-Type", "text/html");
-  fs.readFile("./views/index.html", (error, data) => {
+
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    default:
+      path += "404.html";
+      break;
+  }
+
+  fs.readFile(path, (error, data) => {
     if (error) {
       console.log(error);
+      resp.end();
     } else {
       resp.write(data);
       resp.end();
