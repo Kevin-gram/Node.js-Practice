6404d7690d92a34e6c96bdfe79d245fc2cf18aec
Kevin-gram <k.nyiringan@alustudent.com>
2024-08-22 12:26:28 +0200

    using express for routing

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const express = require("express");
 const app = express();
 app.listen(3000);
 app.get("/", (req, resp) => {
-  resp.send("./views/index.html", { root: __dirname });
+  resp.sendFile("./views/index.html", { root: __dirname });
 });
