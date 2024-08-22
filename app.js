c2a5a48a23c2c3c536e94802816611ae5abbd7a9
Kevin-gram <k.nyiringan@alustudent.com>
2024-08-22 12:40:03 +0200

    using express for routing

--- app.js
+++ app.js
@@ -4,3 +4,6 @@ app.listen(3000);
 app.get("/", (req, resp) => {
   resp.sendFile("./views/index.html", { root: __dirname });
 });
+app.get("/about", (req, resp) => {
+  resp.sendFile("./views/about.html", { root: __dirname });
+});
