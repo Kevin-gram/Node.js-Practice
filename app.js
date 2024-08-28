abf8e5e0a734ec0ce783137c819031b62afd080f
Kevin-gram <k.nyiringan@alustudent.com>
2024-08-28 05:20:26 +0200

    routing using ejs

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
+app.set("view engine", "ejs");
 app.listen(3000);
 app.get("/", (req, resp) => {
-  resp.sendFile("./views/index.html", { root: __dirname });
+  resp.render("index");
 });
 app.get("/about", (req, resp) => {
-  resp.sendFile("./views/about.html", { root: __dirname });
+  resp.render("about");
 });
 app.get("/about-us", (req, resp) => {
-  resp.redirect("/about");
+  resp.render("about");
+});
+app.use((req, resp) => {
+  resp.status(404).render("404");
 });
