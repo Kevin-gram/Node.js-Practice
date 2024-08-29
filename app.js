f177ef11d1c5c3a355c28d3f04f6558d71d9e4e7
Kevin-gram <k.nyiringan@alustudent.com>
2024-08-29 08:30:59 +0200

    creating database

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+const dbUri = "mongodb+srv://Nodejs:chico1234@nodejs.vbkfa.mongodb.net/";
+mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
 app.set("view engine", "ejs");
 app.listen(3000);
 app.get("/", (req, resp) => {
